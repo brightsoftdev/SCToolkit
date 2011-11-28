fa5359cdf407a566cd2a4327ec59ff5b7ea4cdd5
smokyonion <smokyonion@gmail.com>
2011-11-28 11:24:50 -0800

    Add NSTimer+Pausing

---
+++ SCToolkit/Additions/NSTimer+Pausing.h
@@ -0,0 +1,16 @@
+//
+//  NSTimer+Pausing.h
+//  SCToolkit
+//
+//  Created by Vincent Wang on 11/28/11.
+//  Copyright (c) 2011 Vincent S. Wang. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface NSTimer (Pausing)
+
+- (void)pause;
+- (void)resume;
+
+@end
