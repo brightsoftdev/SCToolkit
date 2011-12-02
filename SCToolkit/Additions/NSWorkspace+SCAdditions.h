97434a0bcdca957d4a8e7ce0ee8597f370b8aae5
smokyonion <smokyonion@gmail.com>
2011-12-02 10:31:53 -0800

    Update NSWorkspace+SCAdditions, added more useful methods

--- SCToolkit/Additions/NSWorkspace+SCAdditions.h
+++ SCToolkit/Additions/NSWorkspace+SCAdditions.h
@@ -8,10 +8,83 @@
 
 #import <AppKit/AppKit.h>
 
+// inspired by GDKit
+
+/**
+ * Macro definition of a string you can pass to openSystemPreference,
+ * for universal access.
+ */
+#define kSCSystemPrefencesAccounts @"Accounts.prefPane"
+#define kSCSystemPrefencesAppearance @"Appearance.prefPane"
+#define kSCSystemPrefencesBluetooth @"Bluetooth.prefPane"
+#define kSCSystemPrefencesDateAndTime @"DateAndTime.prefPane"
+#define kSCSystemPrefencesDesktopScreenEffectsPref @"DesktopScreenEffectsPref.prefPane"
+#define kSCSystemPrefencesDigiHubDiscs @"DigiHubDiscs.prefPane"
+#define kSCSystemPrefencesDisplays @"Displays.prefPane"
+#define kSCSystemPrefencesDock @"Dock.prefPane"
+#define kSCSystemPrefencesEnergySaver @"EnergySaver.prefPane"
+#define kSCSystemPrefencesExpose @"Expose.prefPane"
+#define kSCSystemPrefencesFibreChannel @"FibreChannel.prefPane"
+#define kSCSystemPrefencesInk @"Ink.prefPane"
+#define kSCSystemPrefencesKeyboard @"Keyboard.prefPane"
+#define kSCSystemPrefencesLocalization @"Localization.prefPane"
+#define kSCSystemPrefencesMobileMe @"MobileMe.prefPane"
+#define kSCSystemPrefencesMouse @"Mouse.prefPane"
+#define kSCSystemPrefencesNetwork @"Network.prefPane"
+#define kSCSystemPrefencesParentalControls @"ParentalControls.prefPane"
+#define kSCSystemPrefencesPrintAndFax @"PrintAndFax.prefPane"
+#define kSCSystemPrefencesSecurity @"Security.prefPane"
+#define kSCSystemPrefencesSharingPref @"SharingPref.prefPane"
+#define kSCSystemPrefencesSoftwareUpdate @"SoftwareUpdate.prefPane"
+#define kSCSystemPrefencesSound @"Sound.prefPane"
+#define kSCSystemPrefencesSpeech @"Speech.prefPane"
+#define kSCSystemPrefencesSpotlight @"Spotlight.prefPane"
+#define kSCSystemPrefencesStartupDisk @"StartupDisk.prefPane"
+#define kSCSystemPrefencesTimeMachine @"TimeMachine.prefPane"
+#define kSCSystemPrefencesTrackpad @"Trackpad.prefPane"
+#define kSCSystemPrefencesUniversalAccess @"UniversalAccessPref.prefPane"
+
 @interface NSWorkspace (SCAdditions)
 
 + (NSWorkspace *)sc_threadSafeWorkspace;
 - (NSImage *)sc_iconForAppWithBundleIdentifier:(NSString *)bundleID;
 - (NSImage *)sc_iconForFile:(NSString *)path size:(NSSize)size;
 
+/**
+ * Opens the system preferences panel and opens the specified
+ * preference. Pass in an argument like "UniversalAccessPref"
+ * this function will append the ".prefpane", and also search
+ * in two places for the prefpane: /System/Library/PreferencePanes/,
+ * and ~/Library/PreferencePanes/
+ */
+- (void)sc_openSystemPreference:(NSString *)preferencesFileName;
+
+/**
+ * Brings an application to the front. Requires an application
+ * info dictionary, which must contain the key "NSApplicationPath"
+ */
+- (void)sc_bringApplicationToFront:(NSDictionary *)appInfo;
+
+/**
+ * Brings the current application to front - uses NSBundle internally.
+ */
+- (void)sc_bringCurrentApplicationToFront;
+
+/**
+ * Brings an application to the front, from an application
+ * path.
+ */
+- (void)sc_bringApplicationToFrontFromPath:(NSString *)appPath;
+
+/**
+ * Installs a launch agent/startup item into ~/Library/LaunchAgents, you
+ * need to pass it a "plist" file. ex: "MyAppStartup.Agent.plist"
+ */
+- (void)sc_installStartupLaunchdItem:(NSString *)plistName;
+
+/**
+ * Removes a launchd agent/startup item from ~/Library/LaunchAgents, you
+ * need to pass it a "plist" file. ex: "MyAppStartup.Agent.plist"
+ */
+- (void)sc_uninstallStartupLaunchdItem:(NSString *)plistName;
 @end
--- SCToolkit/Additions/NSWorkspace+SCAdditions.m
+++ SCToolkit/Additions/NSWorkspace+SCAdditions.m
@@ -56,4 +56,59 @@
 	return nil;
 }
 
+- (void)sc_openSystemPreference:(NSString *)preferencesFileName 
+{
+	NSFileManager *fm = [NSFileManager defaultManager];
+	if ([fm fileExistsAtPath:[@"/System/Library/PreferencePanes/" stringByAppendingString:preferencesFileName]]) {
+		[[NSWorkspace sharedWorkspace] openFile:[@"/System/Library/PreferencePanes/" stringByAppendingString:preferencesFileName]];
+	} 
+	else if ([fm fileExistsAtPath:[@"~/Library/PreferencePanes/" stringByAppendingString:preferencesFileName]]) {
+		[[NSWorkspace sharedWorkspace] openFile:[@"~/Library/PreferencePanes/" stringByAppendingString:preferencesFileName]];
+	}
+}
+
+- (void)sc_bringApplicationToFront:(NSDictionary *)appInfo 
+{
+	if (appInfo == nil) return;
+	NSString *appath = [appInfo valueForKey:@"NSApplicationPath"];
+	if (appath == nil) return;
+	[self launchApplication:appath];
+}
+
+- (void)sc_bringCurrentApplicationToFront 
+{
+	[self launchApplication:[[NSBundle mainBundle] bundlePath]];
+}
+
+- (void)sc_bringApplicationToFrontFromPath:(NSString *)appPath 
+{
+	if (!appPath) return;
+	[self launchApplication:appPath];
+}
+
+- (void)sc_uninstallStartupLaunchdItem:(NSString *)plistName 
+{
+	NSFileManager *fm = [NSFileManager defaultManager];
+	NSString *file = [[@"~/Library/LaunchAgents" stringByExpandingTildeInPath] stringByAppendingString:[@"/" stringByAppendingString:plistName]];
+	[fm removeItemAtPath:file error:NULL];
+}
+
+- (void)sc_installStartupLaunchdItem:(NSString *)plistName 
+{
+	if (!plistName) return;
+	NSBundle *bundle = [NSBundle mainBundle];
+	NSFileManager *fm = [NSFileManager sc_threadSafeManager];
+	NSMutableDictionary *attrs = [[NSMutableDictionary alloc] init];
+	[attrs setObject:[NSNumber numberWithUnsignedLong:448] forKey:NSFilePosixPermissions];
+	NSString *path = [bundle pathForResource:[plistName stringByDeletingPathExtension] ofType:@"plist"];
+	NSMutableDictionary *plist = [[NSMutableDictionary alloc] initWithContentsOfFile:path];
+	NSString *executablepath = [bundle executablePath];
+	[plist setObject:executablepath forKey:@"Program"];
+	NSString *dest = [[@"~/Library/LaunchAgents" stringByExpandingTildeInPath] stringByAppendingString:[@"/" stringByAppendingString:plistName]];	
+	[fm createDirectoryAtPath:[@"~/Library/LaunchAgents/" stringByExpandingTildeInPath] withIntermediateDirectories:TRUE attributes:attrs error:nil];
+	[plist writeToFile:dest atomically:YES];
+	[plist release];
+    [attrs release];
+}
+
 @end
