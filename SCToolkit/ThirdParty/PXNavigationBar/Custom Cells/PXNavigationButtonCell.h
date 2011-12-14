494285f6bafff1a2844ed43d425b273d947c1d2f
smokyonion <smokyonion@gmail.com>
2011-12-13 17:10:10 -0800

    Add PXNavigationBar

---
+++ SCToolkit/ThirdParty/PXNavigationBar/Custom Cells/PXNavigationButtonCell.h
@@ -0,0 +1,22 @@
+//
+//  PXNavigationButtonCell.h
+//  PXNavigationBar
+//
+//  Created by Alex Rozanski on 15/02/2010.
+//  Copyright 2010 Alex Rozanski http://perspx.com
+//
+
+#import <Cocoa/Cocoa.h>
+
+typedef enum {
+	PXNavigationButtonCellNavDirectionLeft,
+	PXNavigationButtonCellNavDirectionRight
+} PXNavigationButtonCellNavDirection;
+
+@interface PXNavigationButtonCell : NSButtonCell {
+    NSInteger navDirection;
+}
+
+@property (readwrite) NSInteger navDirection;
+
+@end
